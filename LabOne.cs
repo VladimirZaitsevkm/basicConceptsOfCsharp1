9372e084b4f002d5c6983a04db0ba5eaffa4515e
VladimirZaitsevkm <vladimir.zaitsevvv@gmail.com>
2025-02-15 14:26:20 +0300

    adding initial option menu

---
+++ LabOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOne1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите задание(1 или 2): ");
+            int userChoice = Convert.ToInt16(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                TaskOne();
+            }
+            else if (userChoice == 2)
+            {
+                TaskTwo();
+            }
+            else
+            {
+                Console.WriteLine("Некоректный ввод, убедитесь что вы выбрали правильной номер задания");
+            }
+        }
+    }
+}
